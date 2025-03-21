07c8a4c182e8640d7696f821a80901b403708d10
Daniel Janík <01djanik@gmail.com>
2025-03-21 20:44:58 +0100

    added code samples

---
+++ demos/csharp.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Class : Inherited
+{
+    private int num = 29;
+    private string str = "string";
+    private bool boo = true;
+    public int PropertyInteger {get; set;}
+
+    public void Method(Type obj)
+    {
+        int localVariable = 20;
+        obj.Method();
+        print();
+        Type2 thing = new Type2();
+    }
+}
